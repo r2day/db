1bbda07175928aec25cef62811edb8df3a4ec259
dropthemasquerade <dropthemasquerade@gmail.com>
2023-03-25 11:10:00 +0800

    update models

--- models/MerchantApps.go
+++ models/MerchantApps.go
@@ -35,6 +35,10 @@ type MerchantApps struct {
 
 	// 应用介绍
 	Desc string `json:"desc"`
+
+	// 应用配置
+	// 用于指导前端显示sidebar
+	AppMenu ApplicationMenu `json:"application_menu" bson:"application_menu"`
 }
 
 // MerchantApps 通过id获取对象
