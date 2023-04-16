2c9cdfa2c366af05543dba65c81dfe7e19d02f5e
imac <imac@imacdeiMac.local>
2023-04-16 18:30:15 +0800

    add redis db

--- redis.go
+++ redis.go
@@ -1,8 +1,6 @@
 package db
 
 import (
-	"time"
-
 	"github.com/redis/go-redis/v9"
 
 	log "github.com/sirupsen/logrus"
@@ -14,12 +12,12 @@ var RDB *redis.Client
 // InitRedisDB 初始化redis链接池
 func InitRedisDB(redisAddr string, db int, poolSize int) error {
 	RDB = redis.NewClient(&redis.Options{
-		Addr:        redisAddr,        // Redis地址
-		Password:    "",               // Redis账号
-		DB:          db,               // Redis库
-		PoolSize:    poolSize,         // Redis连接池大小
-		MaxRetries:  3,                // 最大重试次数
-		IdleTimeout: 10 * time.Second, // 空闲链接超时时间
+		Addr:       redisAddr, // Redis地址
+		Password:   "",        // Redis账号
+		DB:         db,        // Redis库
+		PoolSize:   poolSize,  // Redis连接池大小
+		MaxRetries: 3,         // 最大重试次数
+		// IdleTimeout: 10 * time.Second, // 空闲链接超时时间
 	})
 	pong, err := RDB.Ping().Result()
 	if err == redis.Nil {
@@ -34,6 +32,7 @@ func InitRedisDB(redisAddr string, db int, poolSize int) error {
 		return err
 	} else {
 		log.WithField("redisAddr", redisAddr).
+			WithField("pong", pong).
 			WithField("db", db).
 			WithField("poolSize", poolSize).Info("connect redis successful")
 		return nil
